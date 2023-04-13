8e45c6aa331060e082b557b171e4b05291716110
blacktornado2 <blacktornado2108@gmail.com>
2023-04-13 17:54:52 +0530

    Make Banner Component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Banner.css";
+
+const imageURL = import.meta.env.VITE_IMAGE_URL;
+const imageSize = "w500";
+
+const Banner = ({ featuredMovie }) => {
+    console.log(featuredMovie);
+    return (
+        featuredMovie && (
+            <div className="banner">
+                <img
+                    src={`${imageURL}/${imageSize}/${featuredMovie.poster_path}`}
+                    alt=""
+                />
+                <div className="movie-title">{featuredMovie.title}</div>
+                <div className="movie-description">
+                    {featuredMovie.overview}
+                </div>
+            </div>
+        )
+    );
+};
+
+export default Banner;
